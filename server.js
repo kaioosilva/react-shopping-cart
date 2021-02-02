16a961a639957d627e360fc6b1243ab171301550
Kaio <kaioo_silva@outlook.com>
2021-02-02 16:23:48 +0000

    Products backend
    Signed-off-by: Kaio <kaioo_silva@outlook.com>

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortId = require("shortId");
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://kaio:KaioIreland17@cluster0.5xqr3.mongodb.net/react-shopping-cart-db?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const Product = mongoose.model(
+  "products",
+  new mongoose.Schema({
+    _id: { type: String, default: shortId.generate },
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    availableSizes: [String],
+  })
+);
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
+
+app.delete('/api/products/:id', async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Server at http://localhost:5000'));
